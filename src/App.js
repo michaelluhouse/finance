2a47252655f15374949ba8f6421a2f2b75961e03
michaelluhouse <michaelluhouse@outlook.com>
2022-02-09 11:07:08 +0800

    finished the initial structure of the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import HomePage from './homepage.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HomePage />
+        {/* <Route exact path='/' component={HomePage} />
+        {/* <Route path='/1' component={one} />
+        <Route path='/2' component={2} />
+        <Route path='/3' component={3} />
+        <Route path='/4' component={4} />
+        <Route path='/5' component={5} />
+        <Route path='/6' component={6} />
+        <Route path='/7' component={7} />
+        <Route path='/8' component={8} />
+        <Route path='/9' component={9} />
+        <Route path='/10' component={10} />
+        <Route path='/11' component={11} />
+        <Route path='/12' component={12} />
+        <Route path='/13' component={13} />
+        <Route path='/14' component={14} />
+        <Route path='/15' component={15} />
+        <Route path='/16' component={16} />
+        <Route path='/17' component={17} />
+        <Route path='/18' component={18} />
+        <Route path='/19' component={19} />
+        <Route path='/20' component={20} /> */} 
     </div>
   );
 }
---
+++ src/homepage.jsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import './homepage.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>一</h1>
+                    <span className='subtitle'>会计学、财务管理学、经济学与金融学的区别</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>二</h1>
+                    <span className='subtitle'>会计为企业与投资者提供决策依据</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>三</h1>
+                    <span className='subtitle'>企业如何记账——复式记账法与会计准则</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>四</h1>
+                    <span className='subtitle'>财务的基本概念（1）：资产、负债、所有者权益、收入、费用、利润</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>五</h1>
+                    <span className='subtitle'>财务的基本概念（2）：成本、费用与支出</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>六</h1>
+                    <span className='subtitle'>财务的基本概念（3）：无形资产、商誉、折旧与摊销</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>七</h1>
+                    <span className='subtitle'>认识三张财务报表</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>八</h1>
+                    <span className='subtitle'>读懂资产负债表之资产</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>九</h1>
+                    <span className='subtitle'>读懂资产负债表之负债</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十</h1>
+                    <span className='subtitle'>读懂资产负债表之所有者权益</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十一</h1>
+                    <span className='subtitle'>读懂资产负债表之所有者权益的变动</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十二</h1>
+                    <span className='subtitle'>读懂损益表</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十三</h1>
+                    <span className='subtitle'>读懂现金流量表</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十四</h1>
+                    <span className='subtitle'>财务报表分析：概述</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十五</h1>
+                    <span className='subtitle'>财务报表分析：方法与内容</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十六</h1>
+                    <span className='subtitle'>财务造假的动机与手段</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十七</h1>
+                    <span className='subtitle'>如何识别财务造假</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十八</h1>
+                    <span className='subtitle'>什么事EPS、市盈率、市净率</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>十九</h1>
+                    <span className='subtitle'>有价证卷解析</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>二十</h1>
+                    <span className='subtitle'>价值与价值评估方法</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage
